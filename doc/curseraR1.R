6de3259792985b174bfd7fd8d5b4e1753763490f
orozcoae89 <orozcoae89@gmail.com>
2020-07-10 15:02:20 +0200

    final changes

---
+++ doc/curseraR1.R
@@ -0,0 +1,15 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+knitr::opts_chunk$set(eval = FALSE, include = FALSE)
+
+## ----eval=FALSE, include=FALSE------------------------------------------------
+#  fars_data <- fars_read_years(2013)
+
+## ----eval=FALSE, include=FALSE------------------------------------------------
+#  knitr::kable(fars_summarize_years(years = c(2013:2014)))
+
